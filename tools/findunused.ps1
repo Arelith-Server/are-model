2dce96f4b1f254c5ea15ffcc91ec6977cff8dfae
Morderon <will386@gmail.com>
2019-08-26 21:11:12 -0400

    Tools for repo

---
+++ tools/findunused.ps1
@@ -0,0 +1,15 @@
+$dir = $Args[0]
+$filetype = $Args[1]
+
+Get-ChildItem "../$dir/$filetype" -Filter *.$filetype |
+ Foreach-Object {
+    $content =  $_.BaseName
+	#echo $content "../$dir/mdl/*.mdl"
+	#echo "\(texture\|bitmap\) \+\b$content\b"
+	if(-Not (Select-String -path "../$dir/mdl/*.mdl" -pattern "(texture|bitmap) +\b$content\b" -Quiet)) { echo $content }
+	#Get-ChildItem "../$dir/mdl" -Filter *.mdl |
+	# Foreach-Object {
+	#    write Get-Content $_.FullName | Select-String -pattern ".*$content.*" -AllMatches
+     	#if((Get-Content $_.FullName | Select-String -pattern "$content" --AllMatches).length > 0) { echo $content }
+	# }
+}
