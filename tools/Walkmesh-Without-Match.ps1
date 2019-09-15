ded2e4d256d2f6b2c3d8f6a8458a7b458501b0be
Morderon <will386@gmail.com>
2019-09-14 21:04:27 -0400

    Minor fixes.

--- tools/Walkmesh-Without-Match.ps1
+++ tools/Walkmesh-Without-Match.ps1
@@ -1,5 +1,5 @@
 $dir = $Args[0]
-Get-ChildItem ../$dir/walkmesh -Filter *.* |
+Get-ChildItem ../$dir/walkmesh |
  Foreach-Object {
     $content =  $_.BaseName
     if(-Not (Test-Path -Path "../$dir/mdl/$content.mdl"))
